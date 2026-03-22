bbbccec315f285a41ea46b51e7c2cdfe522df8f9
pranavg1804 <pranavg1804@gmail.com>
2026-03-23 00:52:21 +0530

    main.py

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="E-commerce Analytics API")
 # -----------------------------
 def get_db_connection():
     return psycopg2.connect(
-        host="db",
+        host="localhost",
         database="ecommerce",
         user="admin",
         password="admin123",
